cab46254fd7647b93d9b4524883b7442063e1c44
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2023-03-20 20:50:18 +0100

    adding localstorage to extension

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 let inputBtn = document.getElementById("input-btn");
 let mylinks = [];
 const inputEl = document.getElementById("input-el");
-inputBtn.addEventListener("click", () =>{
-    mylinks.push(inputEl.value);
-    inputEl.value = "";
-    console.log("Value pushhed");
-    console.log(mylinks);
-})
+const ulEl = document.getElementById("ul-el");
+let linkFromlocalstorage = JSON.parse(localStorage.getItem("mylinks"));
+
+if (linkFromlocalstorage)
+{
+  mylinks = linkFromlocalstorage;
+  renderLeads();
+}
+
+
+inputBtn.addEventListener("click", () => {
+  mylinks.push(inputEl.value);
+  console.log("Value pushhed");
+  localStorage.setItem("mylinks",JSON.stringify(mylinks))
+  renderLeads();
+  inputEl.value = "";
+});
+
+function renderLeads() {
+    let listItems = "";
+  for (let i = 0; i < mylinks.length; i++)
+    {
+        //listItems += "<li><a href ='"+ mylinks[i] +"'target='_blank'>" + mylinks[i] + "</a></li>";
+        listItems += `
+        <li>
+        <a target='_blank' href ='${mylinks[i]}'>
+        ${mylinks[i]}</a>
+        </li>
+        `;
+
+    }
+   ulEl.innerHTML = listItems;
+}
 
