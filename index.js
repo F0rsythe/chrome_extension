1be50ba14ef44f831256bc57ac8301a8c727679f
Demilade Tunde-Ayinuola <tundeayinuolad@gmail.com>
2023-03-27 07:34:59 +0100

    changed function to use parameters

--- index.js
+++ index.js
@@ -8,32 +8,32 @@ let linkFromlocalstorage = JSON.parse(localStorage.getItem("mylinks"));
 if (linkFromlocalstorage)
 {
   mylinks = linkFromlocalstorage;
-  renderLeads();
+  render(mylinks);
 }
 
 deletebtn.addEventListener("dblclick", ()=>{
   localStorage.clear();
   mylinks = [];
-  renderLeads();
+  render(mylinks);
 });
 
 inputBtn.addEventListener("click", () => {
   mylinks.push(inputEl.value);
   console.log("Value pushhed");
   localStorage.setItem("mylinks",JSON.stringify(mylinks))
-  renderLeads();
+  render(mylinks);
   inputEl.value = "";
 });
 
-function renderLeads() {
+function render(leads) {
     let listItems = "";
   for (let i = 0; i < mylinks.length; i++)
     {
         //listItems += "<li><a href ='"+ mylinks[i] +"'target='_blank'>" + mylinks[i] + "</a></li>";
         listItems += `
         <li>
-        <a target='_blank' href ='${mylinks[i]}'>
-        ${mylinks[i]}</a>
+        <a target='_blank' href ='${leads[i]}'>
+        ${leads[i]}</a>
         </li>
         `;
 
